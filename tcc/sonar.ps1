f2bb63cedb0caf64d702f9ef5498bcca6c96972f
Marlon Schuetz <rodrigoschuetz@gmail.com>
2026-05-07 15:39:22 -0300

    atualização config

--- tcc/sonar.ps1
+++ tcc/sonar.ps1
@@ -1,5 +1,5 @@
 #$sonarToken = Read-Host "Informe a Sonar token"
-$sonarToken = "sqp_31ea512f70a836e5e2cdafa8908ee870156ef5d3"
+$sonarToken = "sqp_0a43e7fda75a0609b0143aee18508cbbdfec26b2"
 
 dotnet sonarscanner begin `
     /k:"TCC-RestSharp-MagicNumberIssue" `
